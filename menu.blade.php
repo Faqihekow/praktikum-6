ca2940295e35cbb262a73b68f6f7e32494937584
Faqihekow <90909165+Faqihekow@users.noreply.github.com>
2021-11-02 20:45:13 +0700

    Add files via upload

---
+++ menu.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<table border="1" width="300">
+ <caption>Menu Makanan</caption>
+ <thead>
+  <tr>
+   <td>No</td>
+   <td>Makanan</td>
+   <td>Minuman</td>
+  </tr>
+ </thead>
+ <tbody>
+  <tr>
+   <td>1</td>
+   <td>1. Nasi & Ikan Nila Bakar</td>
+   <td>1. Es Teh</td>
+  </tr>
+  <tr>
+   <td>2</td>
+   <td>2. Nasi & Ikan Lele Bakar</td>
+   <td>2. Es Jeruk</td>
+  </tr>
+  <tr>
+   <td>3</td>
+   <td>3. Nasi & Ayam Goreng</td>
+   <td>3. Jeruk Panas</td>
+  </tr>
+  <tr>
+   <td>4</td>
+   <td>4. Nasi & Bebek Goreng</td>
+   <td>4. Teh Panas</td>
+  </tr>
+ </tbody>
+</table>
+<label>Silahkan Pilih Menu Makanan : </label>
+                <input type="checkbox" name="menu" value="a">Nasi
+                <input type="checkbox" name="menu" value="b">Ikan Nila Bakar
+                <input type="checkbox" name="menu" value="c">Ikan Lele Bakar
+                <input type="checkbox" name="menu" value="d">Ayam Goreng
+                <input type="checkbox" name="menu" value="e">Bebek Goreng
+                <input type="checkbox" name="menu" value="e">Es Teh
+                <input type="checkbox" name="menu" value="e">Es Jeruk
+                <input type="checkbox" name="menu" value="e">Jeruk Panas
+                <input type="checkbox" name="menu" value="e">Teh Panas
+            </label><br><br>
+            <input type="submit" value="Kirim">
+</head>
+<body>
+    
+</body>
+</html>
